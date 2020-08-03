6c653e781499ede11c5831d920dc2431a05ea4be
jazzwang <jazzwang.tw@gmail.com>
2020-08-03 13:35:24 +0800

    [mysql-cdc-fluentd-files] add demo.sh

---
+++ mysql-cdc-fluentd-files/README.md
@@ -0,0 +1,7 @@
+# README
+
+## Reference
+
+- [fluent-plugin-mysql-replicator](https://github.com/y-ken/fluent-plugin-mysql-replicator)
+- [official mysql docker image](https://hub.docker.com/_/mysql)
+- [mysql commandline password file](https://stackoverflow.com/questions/34916074/how-to-pass-password-from-file-to-mysql-command)
---
+++ mysql-cdc-fluentd-files/demo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+docker-compose up -d
+echo "[31;1m <INFO> Wait 15 seconds to initiate MySQL Database ...[0m"
+sleep 15
+echo "[31;1m <INFO> Checking MySQL status ...[0m"
+docker logs mysql | grep "Entrypoint"
+echo "[31;1m <INFO> Checking Fluentd status ...[0m"
+docker logs fluentd | tail -n 10
+echo "[32;1m <INFO> 1st process to insert 999 records in 'myweb.search_test' table ...[0m"
+docker exec mysql /app/gen-data.sh &
+jobs
+sleep 10
+echo "[31;1m <INFO> Checking Fluentd status ...[0m"
+docker exec fluentd ls /tmp/buffer
+echo "[32;1m <INFO> update 3 records in 'myweb.search_test' table ...[0m"
+docker exec mysql /app/update-data.sh
+sleep 10
+docker logs fluentd | grep --color "update"
+echo "[31;1m <INFO> 2nd process to insert 999 records in 'myweb.search_test' table ...[0m"
+docker exec mysql /app/gen-data.sh &
+jobs
+sleep 10
+echo "[31;1m <INFO> 3rd process to insert 999 records in 'myweb.search_test' table ...[0m"
+docker exec mysql /app/gen-data.sh &
+jobs
+sleep 10
+echo "[32;1m <INFO> update 6 records in 'myweb.search_test' table ...[0m"
+docker exec mysql /app/update-data.sh
+sleep 10
+docker logs fluentd | grep --color "update"
+sleep 10
+docker exec fluentd ls /tmp/buffer
+echo "[35;1m <INFO> show fluentd output gz files ...[0m"
+sleep 10 
+ls -alR fluentd/output
+jobs
+sleep 10 
+docker-compose down
